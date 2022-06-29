db44319d87732a63d10a311c54da661610a3442d
Bora Guven <guvenbe@gmail.com>
2022-06-29 15:30:16 -0400

    first commit

---
+++ hello-world.js
@@ -0,0 +1,12 @@
+console.log("hello world Learning JS :)")
+console.log('Ola Mundo')
+
+console.log(10+10)
+/*
+*
+*
+*/
+var a = 10
+var b =70
+b=5
+
---
+++ varaibles.js
@@ -0,0 +1,34 @@
+var age = 21
+var firstName= "Bora"
+var isFemale = true
+var balance =100.3
+var dob = new Date(2000, 0, 30)
+var person ={}
+var empty = undefined
+console.log(firstName)
+console.log(age)
+console.log(isFemale)
+console.log(balance)
+console.log(dob)
+console.log(person)
+console.log(empty)
+
+console.log("*****************")
+console.log(typeof firstName)
+console.log(firstName.length)
+console.log(firstName.toUpperCase())
+console.log(firstName.substring(0,2))
+
+console.log(typeof age)
+console.log(typeof isFemale)
+console.log(typeof balance)
+console.log(typeof dob)
+console.log(typeof person)
+console.log(typeof empty)
+
+var a = "Bora"
+var b = "Guven"
+console.log(a + " " + b)
+console.log(`${a} ${b}`)
+console.log(`${a} ${b.toUpperCase()}`)
+
