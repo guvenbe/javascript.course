733aa0e2f3b16ebbc7db9d107c486f63a27d4ebb
Bora Guven <guvenbe@gmail.com>
2022-06-30 01:35:56 -0400

    switch

---
+++ switch.js
@@ -0,0 +1,13 @@
+var gender = 'F'
+
+switch(gender){
+    case 'M':
+        console.log('Male')
+        break
+    case 'F':
+        console.log("Female")
+        break
+    default:
+        console.log("unknown");
+
+}
