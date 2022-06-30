b46258f37e54b63c023c821c2d72ba6c02f2d501
Bora Guven <guvenbe@gmail.com>
2022-06-29 22:16:07 -0400

    arithmetic

---
+++ arithmetic-operators.js
@@ -0,0 +1,13 @@
+var addition = 2 +2;
+var substraction = 2 -2;
+var division = 10 /2;
+var multiplication = 10 * (2+1) *0
+var remainder = 10 % 7
+var exponentation = 3 ** 4
+
+console.log(addition)
+console.log(substraction)
+console.log(division)
+console.log(multiplication)
+console.log(remainder)
+console.log(exponentation)
