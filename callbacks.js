568f18d2bd278765edb99d959872f8e254c7bb5d
Bora Guven <guvenbe@gmail.com>
2022-07-06 22:11:14 -0400

    callbacks

---
+++ callbacks.js
@@ -0,0 +1,22 @@
+//callback a function that is passed to a function as parameter
+
+const greetUser = (username, callback) =>{
+    if(callback) {
+        console.log(callback(username));
+    } else {
+        console.log(username)
+    }
+    
+}
+
+const cb =  username =>{
+    return "hello " + username
+}
+
+greetUser("james", cb)
+
+//or
+
+greetUser("Bob", (username) =>{
+    return "Good bye " + username
+})
